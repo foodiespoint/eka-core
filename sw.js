97ac69292ff5dc41306edb3bf947e968d240343f
foodiespoint <prime.aksh@gmail.com>
2026-08-11 00:14:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,39 @@
-const CACHE_NAME = 'eka-core-v3
-';
-const ASSETS = [
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'eka-core-v4';
 
+// Install event - caches essential files and immediately activates
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(['./', './index.html']);
+        })
+    );
 });
 
+// Activate event - deletes all old caches (v1, v2, v3, etc.)
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// Fetch event - network-first strategy to always get fresh index.html
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        fetch(e.request)
+            .then((response) => {
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(e.request, responseClone));
+                return response;
+            })
+            .catch(() => caches.match(e.request))
+    );
 });
